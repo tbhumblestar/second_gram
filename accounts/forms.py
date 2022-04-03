306eefde4790c5f5acc0f6953ecfc6599d2a156c
humblebee <colock123@gmail.com>
2022-04-04 01:33:23 +0900

    3:Usermodel creation and signup-normal

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,12 +12,14 @@ class SignupForm(UserCreationForm):
         self.fields['email'].required = True
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
+        self.fields['phone_number'].required = True
     
     class Meta(UserCreationForm.Meta):
         #Meta도 usercreationform의 메타이기 때문
         model = get_user_model()
         #model도 직접적으로 설정을 해줘야 함. auth에 있는 모델에 의존하고 있기 때문
-        fields = ['username','email','first_name','last_name']
+        fields = ['username','email','first_name','last_name','phone_number']
+
 
         #form이 제출될 때, clean_field형식의 함수들이 자동으로 호출이 됨? 
         #답변 form.cleaned_data는 유효성 검사가 시작되는 시점에 빈 사전으로 초기화가 되며, 유효성 검사 과정에서 각 필드에 대한 값들이 업데이트됨
@@ -28,5 +30,13 @@ class SignupForm(UserCreationForm):
             if qs.exists():
                 raise forms.ValidationError("이미 등록된 이메일 주소입니다.")
         return email
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if phone_number:
+            qs = User.objects.filter(phone_number=phone_number)
+            if qs.exists():
+                raise forms.ValidationError("이미 등록된 전화번호 입니다")
+        return phone_number
         
         
--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     website_url = models.URLField(blank=True, max_length=200)
     bio = models.TextField(blank=True)
-    only_for_testing = models.TextField(blank=True)
-
+    phone_number = models.DecimalField(blank=True,max_digits=12,decimal_places=0)
